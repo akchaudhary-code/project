884c74d15cba1a845ed46b2f66ad361ce0be80f9
akchaudhary-code <anuragk23369@gmail.com>
2025-10-24 19:43:49 +0530

    Bug fixes

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
+const btn = document.querySelector("button");
 
+btn.addEventListener("click", function(){
+    window.open("learn.html", "_blank");
+    console.log("Button clicked, learn.html file opened.");
+})
